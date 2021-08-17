a8646bfea05dc3714952bf72896eb831089d6d85
Federico Scheu <fscheu@gmail.com>
2021-08-17 17:42:19 -0300

    form con campos

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'boot_play.apps.BootPlayConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
